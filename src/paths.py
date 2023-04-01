2782cb4f02a300c98503ea09f5167952eae59bf6
willnovus <gabrielwill666@gmail.com>
2023-04-01 18:45:07 +0100

    updating repository to set up github actions

---
+++ src/paths.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import os
+
+PARENT_DIR = Path(__file__).parent.resolve().parent
+DATA_DIR = PARENT_DIR / 'data'
+RAW_DATA_DIR = PARENT_DIR / 'data'/'raw'
+TRANSFORMED_DATA_DIR = PARENT_DIR / 'data'/'transformed'
+
+if not Path(DATA_DIR).exists():
+    os.mkdir(DATA_DIR)
+
+if not Path(RAW_DATA_DIR).exists():
+    os.mkdir(RAW_DATA_DIR)
+
+if not Path(TRANSFORMED_DATA_DIR).exists():
+    os.mkdir(TRANSFORMED_DATA_DIR)
