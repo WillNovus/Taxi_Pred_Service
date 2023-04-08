ecb2b146b9fc50ba2a1b76d2bc140aa6cfc7b06a
willnovus <gabrielwill666@gmail.com>
2023-04-08 18:16:43 +0100

    added feature and model values for predictions and monitoring

--- src/config.py
+++ src/config.py
@@ -13,4 +13,16 @@ except:
     raise Exception("Create an .env file on the project root with the HOPSWORKS_API_KEY")
 
 FEATURE_GROUP_NAME = "time_series_hourly_feature_group"
-FEATURE_GROUP_VERSION = 1  
+FEATURE_GROUP_VERSION = 1 
+FEATURE_VIEW_NAME = "time_series_hourly_feature_view" 
+FEATURE_VIEW_VERSION = 1
+MODEL_NAME = "taxi_demand_predictor_next_hour"
+MODEL_VERSION = 1
+
+# added for monitoring purposes
+FEATURE_GROUP_MODEL_PREDICTIONS = 'model_predictions_feature_group'
+FEATURE_VIEW_MODEL_PREDICTIONS = 'model_predictions_feature_view'
+FEATURE_VIEW_MONITORING = 'predictions_vs_actuals_for_monitoring_feature_view'
+
+# number of historical values our model needs to generate predictions
+N_FEATURES = 24 * 28
