66af598f2b9de3c54107a352767bf2b3015dd3c3
WillNovus <gabrielwill666@gmail.com>
2024-01-25 12:27:12 +0100

    rearranged imports

--- src/feature_store_api.py
+++ src/feature_store_api.py
@@ -1,29 +1,11 @@
-from typing import Optional, List
-from dataclasses import dataclass
 
 import hsfs
 import hopsworks
-
+from src.config import FeatureGroupConfig, FeatureViewConfig
 import src.config as config
 from src.logger import get_logger
 
 logger = get_logger()
-
-@dataclass
-class FeatureGroupConfig:
-    name: str
-    version: int
-    description: str
-    primary_key: List[str]
-    event_time: str
-    online_enabled: Optional[bool] = False
-
-@dataclass
-class FeatureViewConfig:
-    name: str
-    version: int
-    feature_group: FeatureGroupConfig
-
 
 
 def get_feature_store() -> hsfs.feature_store.FeatureStore:
@@ -38,24 +20,6 @@ def get_feature_store() -> hsfs.feature_store.FeatureStore:
     )
     return project.get_feature_store()
 
-def get_feature_group(
-    name: str,
-    version: Optional[int] = 1
-    ) -> hsfs.feature_group.FeatureGroup:
-    """Connects to the feature store and returns a pointer to the given
-    feature group `name`
-
-    Args:
-        name (str): name of the feature group
-        version (Optional[int], optional): _description_. Defaults to 1.
-
-    Returns:
-        hsfs.feature_group.FeatureGroup: pointer to the feature group
-    """
-    return get_feature_store().get_feature_group(
-        name=config.FEATURE_GROUP_METADATA.name,
-        version=config.FEATURE_GROUP_METADATA.version,
-    )
 
 def get_or_create_feature_group(
     feature_group_metadata: FeatureGroupConfig
@@ -71,12 +35,12 @@ def get_or_create_feature_group(
         hsfs.feature_group.FeatureGroup: pointer to the feature group
     """
     return get_feature_store().get_or_create_feature_group(
-        name=config.FEATURE_GROUP_METADATA.name,
-        version=config.FEATURE_GROUP_METADATA.version,
-        description=config.FEATURE_GROUP_METADATA.description,
-        primary_key=config.FEATURE_GROUP_METADATA.primary_key,
-        event_time=config.FEATURE_GROUP_METADATA.event_time,
-        online_enabled=config.FEATURE_GROUP_METADATA.online_enabled
+        name=feature_group_metadata.name,
+        version=feature_group_metadata.version,
+        description=feature_group_metadata.description,
+        primary_key=feature_group_metadata.primary_key,
+        event_time=feature_group_metadata.event_time,
+        online_enabled=feature_group_metadata.online_enabled
     )
 
 def get_or_create_feature_view(
@@ -90,25 +54,23 @@ def get_or_create_feature_view(
     # get pointer to the feature group
     # from src.config import FEATURE_GROUP_METADATA
     feature_group = feature_store.get_feature_group(
-        name=config.FEATURE_VIEW_METADATA.feature_group.name,
-        version=config.FEATURE_VIEW_METADATA.feature_group.version
+        name=feature_view_metadata.feature_group.name,
+        version=feature_view_metadata.feature_group.version
     )
 
     # create feature view if it doesn't exist
     try:
         feature_store.create_feature_view(
-            name=config.FEATURE_VIEW_METADATA.name,
-            version=config.FEATURE_VIEW_METADATA.version,
+            name=feature_view_metadata.name,
+            version=feature_view_metadata.version,
             query=feature_group.select_all()
         )
     except:
         logger.info("Feature view already exists, skipping creation.")
     
     # get feature view
-    feature_store = get_feature_store()
     feature_view = feature_store.get_feature_view(
-        name=config.FEATURE_VIEW_METADATA.name,
-        version=config.FEATURE_VIEW_METADATA.version,
+        name=feature_view_metadata.name,
+        version=feature_view_metadata.version,
     )
-
     return feature_view
